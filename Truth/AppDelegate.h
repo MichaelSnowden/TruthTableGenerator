c514b86d1a2b6882dea1338d085728afc2614cce
michael <michaelosnowden@gmail.com>
2014-09-19 19:13:05 -0700

    Initial Commit

---
+++ Truth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Truth
+//
+//  Created by Michael Snowden on 9/19/14.
+//  Copyright (c) 2014 MichaelSnowden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
